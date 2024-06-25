8f92214d3f00dfefbc24fbcc9c35083253d333ae
AS ABIR HASAN <133064937+ABirHAsan123-beep@users.noreply.github.com>
2024-06-26 02:18:18 +0300

    Update website.py

---
+++ website.py
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0" xml:base="https://www.redhat.com/en/rss/blog">
+    <channel>
+        <title>Red Hat Blog</title>
+        <link>https://www.redhat.com/en/rss/blog</link>
+        <description>Red Hat Blog</description>
+        <language>en</language>
+                    <atom:link href="https://www.redhat.com/en/rss/blog" rel="self" type="application/rss+xml"/>
+                            <copyright>Copyright ©2024 Red Hat, Inc.</copyright>
+            <dc:rights>Copyright ©2024 Red Hat, Inc.</dc:rights>
+                                                    <item>
+                    <title>The Marvin Attack</title>
+                    <link>https://www.redhat.com/en/blog/marvin-attack</link>
+                    <description>The Marvin Attack is a new side-channel attack on cryptographic implementations of RSA in which the attacker decrypts previously captured ciphertext by measuring, over a network, server response times to specially crafted messages. The attacker also may forge signatures with the same key as the one used for decryption. Red Hat published the principles and technical background of the Marvin Attack in September of 2023.Since that time, we have identified lots of other vulnerable implementations and have shipped fixes. Note that most of the CVEs in applications that use OpenSSL have only received</description>
+                    <pubDate>Mon, 24 Jun 2024 00:00:00 +0000</pubDate>
+                    <guid isPermaLink="true">https://www.redhat.com/en/blog/marvin-attack</guid>
+                    <dc:date>Mon, 24 Jun 2024 00:00:00 +0000</dc:date>
+                </item>
+                            <item>
+                    <title>Passkey is the Future, and the Future is Now with Red Hat Enterprise Linux</title>
+                    <link>https://www.redhat.com/en/blog/passkey-with-rhel</link>
+                    <description>Red Hat Enterprise Linux 9.4 introduces the ability for centrally managed users to authenticate through passwordless authentication with a passkey, meaning it's an enterprise Linux distribution with Fast Identity Online 2 (FIDO2) authentication for centrally managed users! This is all built on the Identity Management solution already in Red Hat Enterprise Linux, but enhances product security by enabling passwordless, Multi-Factor Authentication (MFA), and Single Sign-On (SSO).What is Passkey?A passkey is a FIDO2 compatible device that can be used for user authentication. FIDO2 is an open authe</description>
+                    <pubDate>Mon, 24 Jun 2024 00:00:00 +0000</pubDate>
+                    <guid isPermaLink="true">https://www.redhat.com/en/blog/passkey-with-rhel</guid>
+                    <dc:date>Mon, 24 Jun 2024 00:00:00 +0000</dc:date>
+                </item>
+                            <item>
+                    <title>Customize your Red Hat OpenShift nodes and keep them updated</title>
+                    <link>https://www.redhat.com/en/blog/openshift-intro-on-cluster-layering</link>
+                    <description>Today we're excited to announce a new mechanism for admins to safely and easily customize an operating system deployment with highly refined needs while taking full advantage of the automation and power provided by Red Hat OpenShift. This means you don't have to second guess the need for special device drivers for uncommon hardware, system agents, or organizational demands that require more control over your host operating system.Red Hat OpenShift is designed to run on a wide variety of hardware and operational contexts. OpenShift runs so well in a variety of environments that admins rarely ne</description>
+                    <pubDate>Mon, 24 Jun 2024 00:00:00 +0000</pubDate>
+                    <guid isPermaLink="true">https://www.redhat.com/en/blog/openshift-intro-on-cluster-layering</guid>
+                    <dc:date>Mon, 24 Jun 2024 00:00:00 +0000</dc:date>
+                </item>
+                            <item>
+                    <title>Buzzwords unite: 6 reasons to run AI at the edge</title>
+                    <link>https://www.redhat.com/en/blog/buzzwords-unite-6-reasons-run-ai-edge</link>
+                    <description>You can’t scroll very far without seeing buzzwords like artificial intelligence (AI), machine learning (ML), and edge computing. But what happens when we combine these concepts to create AI at the edge? And should we combine them? The benefits of running AI in distributed edge computing environments can be enormous for use cases such as manufacturing, retail, transportation, logistics, smart cities, and more. The list is endless! In this blog post, we explore a few reasons why you should be using AI at the edge.What is data at the edge?Data at the edge includes IoT generated data, user gener</description>
+                    <pubDate>Mon, 24 Jun 2024 00:00:00 +0000</pubDate>
+                    <guid isPermaLink="true">https://www.redhat.com/en/blog/buzzwords-unite-6-reasons-run-ai-edge</guid>
+                    <dc:date>Mon, 24 Jun 2024 00:00:00 +0000</dc:date>
+                </item>
+                            <item>
+                    <title>Automate actions from Datadog observability data</title>
+                    <link>https://www.redhat.com/en/blog/automate-actions-datadog-observability-data</link>
+                    <description>In a digital business world, your applications drive results for both internal employees and external audiences such as customers, partners and suppliers. All of this depends on a resilient, reliable technology stack and as a result observability practices have arisen to provide real-time intelligence about what is happening in essential digital application environments. What if you could take action from this intelligence to quickly resolve alerts quickly before they result in an outage, threat or slow down in the application? Better yet, what if you could also specify the type of alert you w</description>
+                    <pubDate>Mon, 24 Jun 2024 00:00:00 +0000</pubDate>
+                    <guid isPermaLink="true">https://www.redhat.com/en/blog/automate-actions-datadog-observability-data</guid>
+                    <dc:date>Mon, 24 Jun 2024 00:00:00 +0000</dc:date>
+                </item>
+                            <item>
+                    <title>Friday Five — June 21, 2024</title>
+                    <link>https://www.redhat.com/en/blog/friday-five-june-21-2024-red-hat</link>
+                    <description> Red Hat Achieves Major Milestone for In-Vehicle Linux with Functional Safety Assessment and Certification for Linux Math LibraryThe Linux math library, a fundamental component of Red Hat In-Vehicle Operating System, has achieved a functional safety certification from exida, a global leader in functional safety and cybersecurity certification. This milestone underscores Red Hat's pioneering role in obtaining Safety Element out of Context certification for Linux in automotive. Learn more  ComputerWeekly - Best practices to beat container misconfigurationWhile misconfigured containers are a majo</description>
+                    <pubDate>Fri, 21 Jun 2024 00:00:00 +0000</pubDate>
+                    <guid isPermaLink="true">https://www.redhat.com/en/blog/friday-five-june-21-2024-red-hat</guid>
+                    <dc:date>Fri, 21 Jun 2024 00:00:00 +0000</dc:date>
+                </item>
+                            <item>
+                    <title>Red Hat levels up AI support for telecommunication service providers</title>
+                    <link>https://www.redhat.com/en/blog/red-hat-levels-ai-support-telecommunication-service-providers</link>
+                    <description>Earlier this year, Chris Wright reflected on what comes next for telecommunication service providers in 2024 and with these initiatives in mind, we learned that a focus for telcos will be to embrace intelligent automation and AI technologies with an end goal of improving overall business and operational efficiency across network, security, infrastructure and applications in a hybrid environment. Since then, Red Hat has released a number of exciting projects at Red Hat Summit 2024 including the introduction of Red Hat Enterprise Linux (RHEL) AI, enhancements to Red Hat OpenShift AI and more tha</description>
+                    <pubDate>Tue, 18 Jun 2024 00:00:00 +0000</pubDate>
+                    <guid isPermaLink="true">https://www.redhat.com/en/blog/red-hat-levels-ai-support-telecommunication-service-providers</guid>
+                    <dc:date>Tue, 18 Jun 2024 00:00:00 +0000</dc:date>
+                </item>
+                            <item>
+                    <title>Introducing Ansible Lightspeed playbook generation and on-premise deployments</title>
+                    <link>https://www.redhat.com/en/blog/introducing-ansible-lightspeed-playbook-generation-and-premise-deployments</link>
+                    <description>This year, we've launched a crop of new capabilities for Red Hat Ansible Lightspeed. In February, we announced model customization as well as flexible new pricing options, a free trial experience and an administrative dashboard. This month, IBM released a new version of  watsonx Code Assistant that includes full Ansible Playbook generation as well as an on-premise deployment option.Ansible Playbook generationWith Ansible Lightspeed, users can now generate entire Ansible Playbooks from a single natural language prompt. Enhanced interfaceAnsible Lightspeed also includes a completely reimagined g</description>
+                    <pubDate>Tue, 18 Jun 2024 00:00:00 +0000</pubDate>
+                    <guid isPermaLink="true">https://www.redhat.com/en/blog/introducing-ansible-lightspeed-playbook-generation-and-premise-deployments</guid>
+                    <dc:date>Tue, 18 Jun 2024 00:00:00 +0000</dc:date>
+                </item>
+                            <item>
+                    <title>Upskill on OpenShift AI with tailored Red Hat Training and Certification</title>
+                    <link>https://www.redhat.com/en/blog/upskill-on-openshift-ai-with-tailored-redhat-training-and-certification</link>
+                    <description>For a long time, AI was mainly a playground for researchers and scientists. The focus was on creating tools for researchers to build better models and explore new areas rather than operationalizing AI. To make AI work in the real world, we must focus on security, responsibility, scalability, and reproducibility, and these are the same principles we apply to any software development project.Red Hat OpenShift AI provides an integrated MLOps platform for building, training, tuning, deploying and monitoring AI-enabled applications and predictive and foundation models at scale across hybrid cloud e</description>
+                    <pubDate>Mon, 17 Jun 2024 00:00:00 +0000</pubDate>
+                    <guid isPermaLink="true">https://www.redhat.com/en/blog/upskill-on-openshift-ai-with-tailored-redhat-training-and-certification</guid>
+                    <dc:date>Mon, 17 Jun 2024 00:00:00 +0000</dc:date>
+                </item>
+                            <item>
+                    <title>Reducing the significant risk of known exploitable vulnerabilities in Red Hat software</title>
+                    <link>https://www.redhat.com/en/blog/reducing-significant-risk-known-exploitable-vulnerabilities-red-hat-software</link>
+                    <description>In a previous Red Hat article, VP of Red Hat Product Security, Vincent Danen, discussed the question "Do all vulnerabilities really matter?" He emphasized that "a  software vulnerability has the potential to be exploited by miscreants to harm its user." The key word here is "potential". If the potential for exploitation is high, or if an exploit for a vulnerability is already in use in the wild, then these vulnerabilities pose a greater risk and must be prioritized and addressed promptly.Red Hat uses CISA as a source for known exploited vulnerabilitiesThe Cybersecurity and Infrastructure Secur</description>
+                    <pubDate>Mon, 17 Jun 2024 00:00:00 +0000</pubDate>
+                    <guid isPermaLink="true">https://www.redhat.com/en/blog/reducing-significant-risk-known-exploitable-vulnerabilities-red-hat-software</guid>
+                    <dc:date>Mon, 17 Jun 2024 00:00:00 +0000</dc:date>
+                </item>
+                        </channel>
+</rss>
